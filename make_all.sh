50474dede814de07c2bbb0ba70e3043f2db69130
Oleksii Slabchenko <alexslabchenko@gmail.com>
2021-02-10 10:49:04 +0100

    Build libopencm3 if necessary

--- make_all.sh
+++ make_all.sh
@@ -1,8 +1,32 @@
 #! /bin/bash
 
+ORIG_DIR="$(pwd)"
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "$BASE_DIR"
+
+LIBOPENCM3=./libopencm3/lib/libopencm3_stm32f1.a
+
+if [ ! -f "$LIBOPENCM3" ]; then
+    echo "libopencm3 is not yet built"
+    echo "Building libopencm3"
+    cd libopencm3
+    make
+	if [ $? -ne 0 ]
+	then
+	  echo "Build failed"
+	  exit 1
+	fi
+    cd ../
+fi
+
+echo "Building SlAlexUSBLCD"
 make clean
 make Bootloader=1
 make tempclean
 make
 make tempclean
 
+echo "done"
+
+cd $ORIG_DIR
